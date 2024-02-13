fe440c2e8955fef007c8e39aac5af16da17a0fd0
Kristiina Suominen <kristiina.suominen@skillio.ai>
2024-02-13 10:26:22 +0200

    Add function for printing all rows from person

--- src/data/queries.py
+++ src/data/queries.py
@@ -2,18 +2,32 @@ from config import config
 import psycopg2
 
 
-def connect(sql):
+def connect():
     con = None
     try:
         con = psycopg2.connect(**config())
-        cursor = con.cursor()
-        SQL = sql
-        cursor.execute(SQL)
-        row = cursor.fetchone()
-        print(row)
-        cursor.close()
+        print("Connected to the PostgreSQL server.")
+        return con
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
-    finally:
-        if con is not None:
-            con.close()
+
+
+def query_data(sql, how):
+    con = connect()
+    if con is not None:
+        cursor = con.cursor()
+        cursor.execute(sql)
+        if how == "one":
+            row = cursor.fetchone()
+        elif how == "all":
+            row = cursor.fetchall()
+        cursor.close()
+        con.close()
+        return row
+
+
+def all_rows_from_person():
+    sql = """SELECT * FROM person;"""
+    row = query_data(sql, "all")
+    for r in row:
+        print(r)
